8d004ee6f3f80f3c81a1cf9fee9462c1be7b7679
ryotahoshino <75008198+ryotahoshino@users.noreply.github.com>
2024-09-04 14:43:31 +0900

    Jestの導入と設定

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,19 @@
+import { composeConfigs } from 'storybook/internal/preview-api';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  compiler: (() => {
+    if (process.env.NODE_ENV === 'production') {
+      compilerConfig = {
+        ...compilerConfig,
+        reactRemoveProperties: { properties: ['^data-testid$']},
+      }
+    }
+
+    return compilerConfig
+  })(),
+};
 
 
 
